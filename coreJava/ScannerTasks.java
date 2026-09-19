c29e9ab74b54942b09c099700888f56e37193b59
Mathew S <mathewarnold444@gmail.com>
2026-09-19 13:09:45 +0530

    New changes

--- coreJava/ScannerTasks.java
+++ coreJava/ScannerTasks.java
@@ -2,84 +2,115 @@ import java.util.Scanner;
 public class ScannerTasks {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        System.out.println("Enter number 1 :");
-        int num1 = scn.nextInt();
-        System.out.println("Enter number 2 :");
-        int num2 = scn.nextInt();
-        System.out.println("Enter calculation method :");
-        char c1 = scn.next().charAt(0);
-        int total = 0;
+        // System.out.println("Enter number 1 :");
+        // int num1 = scn.nextInt();
+        // System.out.println("Enter number 2 :");
+        // int num2 = scn.nextInt();
+        // System.out.println("Enter calculation method :");
+        // char c1 = scn.next().charAt(0);
+        // int total = 0;
         
-        switch (c1) {
-            case '+' -> total = num1 + num2;
-            case '-' -> total = num1 - num2;
-            case '*' -> total = num1 * num2;
-            case '/' -> total = num1 / num2;
-            default -> System.out.println("Invalid input");
-        }
-        System.out.println("Total :");
-        System.out.println(total);
+        // switch (c1) {
+        //     case '+' -> total = num1 + num2;
+        //     case '-' -> total = num1 - num2;
+        //     case '*' -> total = num1 * num2;
+        //     case '/' -> total = num1 / num2;
+        //     default -> System.out.println("Invalid input");
+        // }
+        // System.out.println("Total :");
+        // System.out.println(total);
 
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("Let's start finding average of number now,");
+        // System.out.println("---------------------------------------------------------------------");
+        // System.out.println("Let's start finding average of number now,");
 
-        System.out.println("Enter the first decimal value");
-        double dc1 = scn.nextDouble();
-        System.out.println("Enter the second decimal value");
-        double dc2 = scn.nextDouble();
-        System.out.println("Enter the third decimal value");
-        double dc3 = scn.nextDouble();
-        System.out.println("Enter the fourth decimal value");
-        double dc4 = scn.nextDouble();
+        // System.out.println("Enter the first decimal value");
+        // double dc1 = scn.nextDouble();
+        // System.out.println("Enter the second decimal value");
+        // double dc2 = scn.nextDouble();
+        // System.out.println("Enter the third decimal value");
+        // double dc3 = scn.nextDouble();
+        // System.out.println("Enter the fourth decimal value");
+        // double dc4 = scn.nextDouble();
 
-        System.out.println("Average of " + dc1 + ", " + dc2 + ", " + dc3 + ", " + dc4 + " is");
-        System.out.println((dc1 + dc2 + dc3 + dc4) / 4);
+        // System.out.println("Average of " + dc1 + ", " + dc2 + ", " + dc3 + ", " + dc4 + " is");
+        // System.out.println((dc1 + dc2 + dc3 + dc4) / 4);
 
 
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("Let's check a person is eligible for marriage");
-        System.out.println("Enter age :");
-        int age = scn.nextInt();
-        if (age >= 21) {
-            System.out.println("Eligible");
-        } else {
-            System.out.println("Not eligible");
-        }
+        // System.out.println("---------------------------------------------------------------------");
+        // System.out.println("Let's check a person is eligible for marriage");
+        // System.out.println("Enter age :");
+        // int age = scn.nextInt();
+        // if (age >= 21) {
+        //     System.out.println("Eligible");
+        // } else {
+        //     System.out.println("Not eligible");
+        // }
 
-        System.out.println("---------------------------------------------------------------------");
-        System.out.println("Enter string input 1 :");
-        String s1 = scn.next();
-        scn.nextLine();
-        System.out.println("Enter string input 2 :");
-        String s2 = scn.nextLine();
-        System.out.println("----------------------");
-        System.out.println(s1);
-        System.out.println(s2);
+        // System.out.println("---------------------------------------------------------------------");
+        // System.out.println("Enter string input 1 :");
+        // String s1 = scn.next();
+        // scn.nextLine();
+        // System.out.println("Enter string input 2 :");
+        // String s2 = scn.nextLine();
+        // System.out.println("----------------------");
+        // System.out.println(s1);
+        // System.out.println(s2);
+
+        // System.out.println("-------------------------------------------------------------------");
+        // System.out.println("Kindly fill the employee details");
+
+        // System.out.println("Enter employee name :");
+        // String name = scn.next();
+        // System.out.println("Enter employee id :");
+        // int id = scn.nextInt();
+        // System.out.println("Enter employee role :");
+        // String role = scn.next();
+        // System.out.println("Enter employee salary :");
+        // int salary = scn.nextInt();
+        // System.out.println("Enter company name :");
+        // String cname = scn.next();
+        // scn.nextLine();
+        // System.out.println("Enter employee address :");
+        // String empaddress = scn.nextLine();
+        // System.out.println("----------------Employee Details----------------");
+        // System.out.println("Employee name: " + name);
+        // System.out.println("Employee id: " + id);
+        // System.out.println("Employee role: " + role);
+        // System.out.println("Salary: " + salary);
+        // System.out.println("Compnay name: " + cname);
+        // System.out.println("Employee Address: " + empaddress);
 
         System.out.println("-------------------------------------------------------------------");
-        System.out.println("Kindly fill the employee details");
+        System.out.println("Kindly fill the inputs");
 
-        System.out.println("Enter employee name :");
-        String name = scn.next();
-        System.out.println("Enter employee id :");
-        int id = scn.nextInt();
-        System.out.println("Enter employee role :");
-        String role = scn.next();
-        System.out.println("Enter employee salary :");
-        int salary = scn.nextInt();
-        System.out.println("Enter company name :");
-        String cname = scn.next();
-        scn.nextLine();
-        System.out.println("Enter employee address :");
-        String empaddress = scn.nextLine();
-        System.out.println("----------------Employee Details----------------");
-        System.out.println("Employee name: " + name);
-        System.out.println("Employee id: " + id);
-        System.out.println("Employee role: " + role);
-        System.out.println("Salary: " + salary);
-        System.out.println("Compnay name: " + cname);
-        System.out.println("Employee Address: " + empaddress);
+        System.out.println("Enter name :");
+        String pName = scn.next();
+        System.out.println("Enter age :");
+        int pAge = scn.nextInt();
+        System.out.println("Enter gender :");
+        String pGender = scn.next();
+        System.out.println("Enter contact number :");
+        Long pContact = scn.nextLong();
+        System.out.println("Enter address :");
+        String pAddress = scn.nextLine();
+        System.out.println("Enter pincode :");
+        int pPincode = scn.nextInt();
+        System.out.println("Enter Taluk :");
+        String pTaluk = scn.next();
+        System.out.println("Enter District :");
+        String pDistrict = scn.next();
+        System.out.println("Enter state :");
+        String pState = scn.next();
 
-
+        System.out.println("---------------Verify the details below----------------");
+        System.out.println("Name: " + pName);
+        System.out.println("Age: " + pAge);
+        System.out.println("Gender: " + pGender);
+        System.out.println("Contact number: " + pContact);
+        System.out.println("Address: " + pAddress);
+        System.out.println("Pincode: " + pPincode);
+        System.out.println("Taluk: " + pTaluk);
+        System.out.println("District: " + pDistrict);
+        System.out.println("State: " + pState);
     }
 }
